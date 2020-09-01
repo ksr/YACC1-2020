64b51ac729a9bc36beaf303f9d4c418eeb86e2c4
Ken Rother <ken.rother@gmail.com>
2020-09-01 15:35:36 -0400

    Add 10 instruction at a time option

--- clocker/clocker.ino
+++ clocker/clocker.ino
@@ -3,8 +3,9 @@
 #define UCODE_CLOCK 12
 #define PULSE_SWITCH 11
 #define INSTRUCTION_SWITCH 10
+#define TEN_INSTRUCTIONS 9
 
-#define CLOCK_TIME 100
+#define CLOCK_TIME 10
 
 unsigned long lastDebounceTime = 0;
 unsigned long debounceDelay = 50;
@@ -13,12 +14,18 @@ void setup() {
   pinMode(CLOCK, OUTPUT);
   pinMode(PULSE_SWITCH, INPUT_PULLUP);
   pinMode(UCODE_CLOCK, INPUT);
-  pinMode(INSTRUCTION_SWITCH, INPUT_PULLUP);
+
+  pinMode(INSTRUCTION_SWITCH, INPUT_PULLUP); 
+  pinMode(TEN_INSTRUCTIONS, INPUT_PULLUP);
 }
 
 void loop() {
   if (!digitalRead(INSTRUCTION_SWITCH)) //no need to debounce
     oneInstruction();
+
+  if (!digitalRead(TEN_INSTRUCTIONS)) //no need to debounce
+    for (int i = 0; i < 10; i++)
+      oneInstruction();
 
   if (!digitalRead(PULSE_SWITCH)) {
     lastDebounceTime = millis();
@@ -33,6 +40,7 @@ void loop() {
 void oneInstruction() {
   while (digitalRead(UCODE_CLOCK) == 0)
     pulseClock(1);
+  pulseClock(1);
 }
 
 void pulseClock(int i) {
