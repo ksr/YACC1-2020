adfa8d832bb620df3a3a96d4c9f0ad3340788927
Ken Rother <ken.rother@gmail.com>
2020-10-27 09:41:13 -0400

    verify pushr popr

--- Software/Sequencer Card/disasm/main.c
+++ Software/Sequencer Card/disasm/main.c
@@ -281,8 +281,8 @@ int main(int argc, char** argv) {
             //printf("Instruction [%d] found\n", i);
         }
     }
-    processInstruction(0x04);
-    processInstruction(0x06);
+    processInstruction(0x07);
+    processInstruction(0x08);
     return (EXIT_SUCCESS);
 }
 
